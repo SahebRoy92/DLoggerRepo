528b88043a72dbe94bce387f13d82c02859fc593
Administrator <administrator@OITs-MacBook-Pro.local>
2018-02-13 12:33:56 +0530

    Initial Commit

---
+++ DLogger/DLogger.h
@@ -0,0 +1,19 @@
+//
+//  DLogger.h
+//  DLogger
+//
+//  Created by Administrator on 13/02/18.
+//  Copyright © 2018 Order of the Light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DLogger.
+FOUNDATION_EXPORT double DLoggerVersionNumber;
+
+//! Project version string for DLogger.
+FOUNDATION_EXPORT const unsigned char DLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLogger/PublicHeader.h>
+
+
---
+++ DLogger/Log.swift
@@ -0,0 +1,21 @@
+//
+//  Log.swift
+//  DLogger
+//
+//  Created by Administrator on 13/02/18.
+//  Copyright © 2018 Order of the Light. All rights reserved.
+//
+
+import Foundation
+import MBProgressHUD
+
+public class Log{
+    public func dLog(_ str : String){
+        let topView = UIApplication.shared.keyWindow?.rootViewController?.view
+        print("Cocoapod --- >>\(str)")
+        MBProgressHUD.showAdded(to: topView!, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            MBProgressHUD.hide(for: topView!, animated: true)
+        }
+    }
+}
